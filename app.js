5e94fcef1b64aec9601637063aad36654bf4da1d
Huzaifa_Habib <cocobutter128@gmail.com>
2023-02-27 10:14:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// //-------------------- Question no 1
+// Array=[""]
+
+// // --------------------Question no 2
+// Array=[""]
+
+// //-------------------- Question no 3
+// Array = ["muhammad hasan",]
+
+// //-------------------- Question no 4 
+// Array =[12,]
+
+// //---------------------Question no 5
+// Array =[true]
+
+// //-------------------- Question no 6
+// Array=["muhammad hasan",15,false]
+
+//-------------------- Question no 7
+let qualification = ["SSC", "HSC", "BSC", "BS", "BCOM", "MS", "M.PHIL", "PHD"]
+
+document.write(`<h1> qualification <h1/> <br>`)
+document.write(`${qualification[0]} <br>`)
+document.write(`${qualification[1]} <br>`)
+document.write(`${qualification[2]} <br>`)
+document.write(`${qualification[3]} <br>`)
+document.write(`${qualification[4]} <br>`)
+document.write(`${qualification[5]} <br>`)
+document.write(`${qualification[6]} <br>`)
+document.write(`${qualification[7]} <br>`)
+
+//-------------------- Question no 8
+var students=["micheal","jhon","tony"]
+var studentScore=[320,230,480]
+document.write(`Score of ${students[0]} is ${studentScore[0]}`)
+
